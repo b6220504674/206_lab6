53c94f15701bbf29aad8dd8f8e1a87e6acf36dfe
Teeraphat-4674 <teeraphat.wu@live.ku.th>
2021-03-18 18:01:13 +0700

    show เกมทายตัวเลข

--- เกมทายตัวเลข.c
+++ เกมทายตัวเลข.c
@@ -1,21 +1,21 @@
 #include<stdio.h>
-#include<math.h>
-#include<string.h>
 int main()
 {
-	int a,b,num,countT=0,countF=0,i;
-	scanf("%d",&num);
-	char a1[num],b2[num];
-	scanf("%d",&a);
-	scanf("%d",&b);
-	itoa(a,a1,10);
-	itoa(b,b2,10);
-	for(i=0;i<strlen(a1);i++)
-	{
-		if(a1[i]==b2[i])
-		countT++;
-		else
-		countF++;
-	}
-	printf("%d %d",countT,countF);
+    int k=0,m=0,j,i;
+    scanf("%d",&j);
+    char x[j],y[j];
+    scanf("%s",x);
+    scanf("%s",y);
+ for ( i = 0; i < j; i++)
+    {
+       if (y[i]==x[i])
+        {
+            k++;
+        }
+        else
+        {
+            m++;
+        }
+    }
+    printf("%d %d",k,m);
 }
